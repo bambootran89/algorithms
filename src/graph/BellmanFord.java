9d304e3f050739bc8bd217e78327e87523579f17
bambootran89 <bambootran89@gmail.com>
2017-11-24 18:51:38 +0800

    adding code for finding negative cycle in graph

--- src/graph/BellmanFord.java
+++ src/graph/BellmanFord.java
@@ -30,10 +30,16 @@ public class BellmanFord {
 	private int N;
 	private ArrayList<Edge> edges;
 	
+	String input;
+	
+	public BellmanFord(String file){
+		input = file;
+	}
+	
 	@SuppressWarnings("unchecked")
 	public void input() throws FileNotFoundException{
 		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(new File("data\\dijkstras"));
+		Scanner scanner = new Scanner(new File(input));
 	    
 	    N = scanner.nextInt();
 	    source = scanner.nextInt();
@@ -50,7 +56,7 @@ public class BellmanFord {
 	    		edges.add(new Edge(i,vertex, dist));
 	    	}
 	    }
-	    System.out.println("edges is " + edges);
+	    System.out.println("\nGraph: edges are " + edges);
 	}
 	
 	public void SolveByBellmanFord(){
@@ -60,21 +66,36 @@ public class BellmanFord {
 		ArrayList<Integer> dad = new ArrayList<>(Arrays.asList(new Integer[N]));
 		Collections.fill(dad, -1);
 		dist.set(source, 0.0);
+		
 		for (int i = 0; i<N; i++){
+			
 			for (Edge edge: edges){
 				if(dist.get(edge.v)  > (dist.get(edge.u) + edge.w)) {
 					dist.set(edge.v , dist.get(edge.u) + edge.w);
 					dad.set(edge.v, edge.u);
+					
 				}
 			}
 		}
 		
+		
 		for (Edge edge: edges){
 			if (dist.get(edge.v) > dist.get(edge.u) + edge.w){
-				System.out.println("There is a negative-weight cycle exists");
+				System.out.print("There is a negative-weight cycle exists: ");
+				
+				int size = 0;
+				for (int i = edge.v; ; i = dad.get(i)){
+					System.out.print(i);
+					if (size > 0 && i == edge.v)
+						break;
+					System.out.print(" <- ");
+					size++;
+				}
+				
 				return;
 			}		
 		}
+		
 		System.out.print("path from Source " + source + " to target " + target + " is: ");
 		if (dist.get(target) < INF){
 			for (int i = target; i!=-1; i = dad.get(i))
@@ -85,8 +106,13 @@ public class BellmanFord {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		BellmanFord dif = new BellmanFord();
+		BellmanFord dif = new BellmanFord("data\\negativeCycleGraph");
 		dif.input();
 		dif.SolveByBellmanFord();
+		
+		dif = new BellmanFord("data\\dijkstras");
+		dif.input();
+		dif.SolveByBellmanFord();
+		
 	}
 }
